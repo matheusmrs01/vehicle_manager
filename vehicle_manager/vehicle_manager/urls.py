bc7d8b3b4b06f5ed2531968c81cdba0f7cad3d5e
Matheus Morais <matheus.silva.morais01@gmail.com>
2023-06-15 14:30:46 -0300

    fix: adicionando rotas para crud de veiculos

--- vehicle_manager/vehicle_manager/urls.py
+++ vehicle_manager/vehicle_manager/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from vehicle.api.viewsets import VehicleViewSet
 
+router = routers.DefaultRouter()
+router.register(r'vehicle', VehicleViewSet)
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
