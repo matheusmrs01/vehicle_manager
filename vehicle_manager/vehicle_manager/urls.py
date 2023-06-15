e14e7b2db80911c37bf5700faa3e29d7debc2ee4
Matheus Morais <matheus.silva.morais01@gmail.com>
2023-06-15 14:31:18 -0300

    fix: adicionando rota para salvar imagens dentro do projeto

--- vehicle_manager/vehicle_manager/urls.py
+++ vehicle_manager/vehicle_manager/urls.py
@@ -16,12 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework import routers
 from vehicle.api.viewsets import VehicleViewSet
 
 router = routers.DefaultRouter()
 router.register(r'vehicle', VehicleViewSet)
+
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
