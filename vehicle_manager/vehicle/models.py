edcf4281b243b519def4f1bbd0526d197bb84591
Matheus Morais <matheus.silva.morais01@gmail.com>
2023-06-15 14:26:40 -0300

    feat: criação do model de veiculo

--- vehicle_manager/vehicle/models.py
+++ vehicle_manager/vehicle/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Vehicle(models.Model):
+    name = models.CharField(max_length=150)
+    brand = models.CharField(max_length=150)
+    model = models.CharField(max_length=150)
+    value = models.FloatField()
+    image = models.ImageField(upload_to='vehicle_images')
+
+    def __str__(self):
+        return self.name + ' - ' + self.brand
+
