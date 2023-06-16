5a9762ac5d237e2a5a6a8e8b2f116710fbfc1adf
Matheus Morais <matheus.silva.morais01@gmail.com>
2023-06-15 23:39:44 -0300

    fix: adicionando busca de veiculos na api

--- vehicle_manager_frontend/src/app/home/home.component.ts
+++ vehicle_manager_frontend/src/app/home/home.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  vehicles: any[] = [];
+  user = null;
 
+  constructor(
+    private apiService: ApiService
+  ) {}
+
+  ngOnInit() {
+    this.apiService.getVehicles().subscribe((result) => {
+      this.vehicles = result
+      console.log(result);
+    }, error => {
+      console.log(error)
+    });
+  }
 }
