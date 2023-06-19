24e103b0c1db392bef5a1d7355e8e362ef09997f
Matheus Morais <matheus.silva.morais01@gmail.com>
2023-06-19 16:46:55 -0300

    fix: adicionando funções para criar e editar um veiculo

--- vehicle_manager_frontend/src/app/api.service.ts
+++ vehicle_manager_frontend/src/app/api.service.ts
@@ -8,16 +8,42 @@ import { Observable } from 'rxjs';
 export class ApiService {
   API_URL = 'http://127.0.0.1:8000';
   headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-  public user = null;
+  key = 'vehicleManagerToken';
 
   constructor(private httpClient: HttpClient) { }
 
-  async login(username: string, senha: string) {
+  login(username: string, senha: string) {
     const body = { 'username': username, 'password': senha };
     return this.httpClient.post<any>(this.API_URL + '/login/', body);
   }
 
-  getVehicles(): Observable<any> {
+  getVehicles(): Observable<any[]> {
     return this.httpClient.get<any[]>(this.API_URL + '/vehicle/');
+  }
+
+  createVehicle(body: any): Observable<any> {
+    return this.httpClient.post(this.API_URL + '/vehicle/', body, {
+      headers: this.getHeader()
+    });
+  }
+
+  updateVehicle(id: string, body: any): Observable<any> {
+    return this.httpClient.put(this.API_URL + `/vehicle/${id}/`, body, {
+      headers: this.getHeader()
+    });
+  }
+
+  getHeader() {
+    return new HttpHeaders().set('Authorization', localStorage.getItem('vehicleManagerToken') as string)
+  }
+
+   // -------------------- LOCAL STORAGE -------------------
+
+   setUserToken(token: string) {
+    localStorage.setItem(this.key, `Token ${token}`);
+  }
+
+  logout() {
+    localStorage.removeItem(this.key);
   }
 }
